bfb6a8454b90abe1bac47cb38e636e4b58513636
eva-freire <freiretrecueva@gmail.com>
2023-08-23 22:48:13 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.getElementById("divBtn").addEventListener("click", () => {
     alert ("Hola, soy el div!");
 } )
+
+function saludar(event){
+    alert('Hola!');
+    event.stopPropagation();
+}
